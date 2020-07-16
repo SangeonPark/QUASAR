0c97e5feb60347f75831adfa831ff91bf67d3b06
SangeonPark <saepark35@gmail.com>
2020-07-16 16:57:28 -0400

    stats

---
+++ statistics/pvalue.py
@@ -0,0 +1,251 @@
+from scipy.optimize import minimize
+from scipy.optimize import curve_fit
+from scipy.stats import poisson, norm, kstest
+from numpy.linalg import inv
+import numpy as np
+import numdifftools
+
+def get_p_value(ydata,binvals,mask=[],verbose=0,plotfile=None,yerr=None,return_teststat = False,plotsys=True,myax=None):
+    ydata = np.array(ydata)
+    #Assume poisson is gaussian with N+1 variance
+    if not yerr:
+        yerr = np.sqrt(ydata+1)
+    else:
+        yerr=np.array(yerr)
+
+    def fit_func(x,p1,p2,p3):
+        #see the ATLAS diboson resonance search: https://arxiv.org/pdf/1708.04445.pdf.
+        xi = 0.
+        y = x/13000.
+        return p1*(1.-y)**(p2-xi*p3)*y**-p3
+
+    xdata = np.array([0.5*(binvals[i]+binvals[i+1]) for i in range(0,len(binvals)-1)])
+    xwidths = np.array([-binvals[i]+binvals[i+1] for i in range(0,len(binvals)-1)])
+
+    #Assuming inputs are bin counts, this is needed to get densities. Important for variable-width bins
+    ydata = np.array(ydata) * 100 / xwidths
+    yerr = np.array(yerr)*100/ np.array(xwidths)
+
+    #Least square fit, masking out the signal region
+    popt, pcov = curve_fit(fit_func, np.delete(xdata,mask), np.delete(ydata,mask),sigma=np.delete(yerr,mask),maxfev=10000)
+    if verbose:
+        print('fit params: ', popt)
+
+    ydata_fit = np.array([fit_func(x,popt[0],popt[1],popt[2]) for x in xdata])
+
+    #Check that the function is a good fit to the sideband
+    residuals = np.delete((ydata - ydata_fit)/yerr,mask)
+
+    if verbose > 0:
+        print("Goodness: ",kstest(residuals, norm(loc=0,scale=1).cdf))
+        print(residuals)
+        print(((ydata - ydata_fit)/yerr)[mask])
+        print('\n')
+
+    #The following code is used to get the bin errors by propagating the errors on the fit params
+    def fit_func_array(parr):
+        #see the ATLAS diboson resonance search: https://arxiv.org/pdf/1708.04445.pdf.
+        p1, p2, p3 = parr
+        xi = 0.
+        return np.array([p1*(1.-(x/13000.))**(p2-xi*p3)*(x/13000.)**-p3 for x in xdata])
+
+    jac=numdifftools.core.Jacobian(fit_func_array)
+    x_cov=np.dot(np.dot(jac(popt),pcov),jac(popt).T)
+    #For plot, take systematic error band as the diagonal of the covariance matrix
+    y_unc=np.sqrt([row[i] for i, row in enumerate(x_cov)])
+
+    if (plotfile != None) & (plotfile != 'ax'):
+        if plotsys:
+            plt.fill_between(xdata,ydata_fit+y_unc,ydata_fit-y_unc,facecolor='gray',edgecolor=None,alpha=0.4)
+        yerr2 = np.array(yerr)
+        yerr2[yerr>=ydata] = yerr2[yerr>=ydata]*0.8
+        plt.errorbar(xdata, ydata,[yerr2,yerr],None, 'bo', label='data',markersize=4)
+        plt.plot(xdata, ydata_fit, 'r--', label='data')
+        plt.yscale('log', nonposy='clip')
+    if plotfile == 'ax':
+        if plotsys:
+            myax.fill_between(xdata,ydata_fit+y_unc,ydata_fit-y_unc,facecolor='gray',edgecolor=None,alpha=0.4)
+        yerr2 = np.array(yerr)
+        yerr2[yerr>=ydata] = yerr2[yerr>=ydata]*0.8
+        myax.errorbar(xdata, ydata,[yerr2,yerr],None, 'bo', label='data',markersize=4)
+        myax.plot(xdata, ydata_fit, 'r--', label='data')
+        myax.set_yscale('log', nonposy='clip')
+    if plotfile == 'show':
+        plt.show()
+    elif plotfile:
+        plt.savefig(plotfile)
+
+    #Now, let's compute some statistics.
+    #  Will use asymptotic formulae for p0 from Cowan et al arXiv:1007.1727
+    #  and systematics procedure from https://cds.cern.ch/record/2242860/files/NOTE2017_001.pdf
+
+    #First get systematics in the signal region
+
+    #This function returns array of signal predictions in the signal region
+    def signal_fit_func_array(parr):
+        #see the ATLAS diboson resonance search: https://arxiv.org/pdf/1708.04445.pdf.
+        p1, p2, p3 = parr
+        xi = 0.
+        return np.array([np.sum([p1*(1.-(x/13000.))**(p2-xi*p3)*(x/13000.)**-p3*xwidths[mask[i]]/100 for i, x in enumerate(xdata[mask])])])
+    #Get covariance matrix of prediction uncertainties in the signal region
+    jac=numdifftools.core.Jacobian(signal_fit_func_array)
+    x_signal_cov=np.dot(np.dot(jac(popt),pcov),jac(popt).T)
+    #Inverse signal region covariance matrix:
+    inv_x_signal_cov = inv(x_signal_cov)
+
+    #Get observed and predicted event counts in the signal region
+    obs = np.array([np.sum(np.array(ydata)[mask]*np.array(xwidths)[mask]/100)])
+    expected = np.array([np.sum([fit_func(xdata[targetbin],popt[0],popt[1],popt[2])*xwidths[targetbin]/100 for targetbin in mask])])
+
+    #Negative numerator of log likelihood ratio, for signal rate mu = 0
+    def min_log_numerator(expected_nuis_arr):
+        #expected_nuis_arr is the array of systematic background uncertainty nuisance parameters
+        #These are event rate densities
+        expected_nuis_arr = np.array(expected_nuis_arr)
+        to_return = 0
+        #Poisson terms
+        for i, expected_nuis in enumerate(expected_nuis_arr):
+            #Poisson lambda. Have to rescale nuisance constribution by bin width
+            my_lambda = expected[i]+expected_nuis_arr[i]
+            #Prevent negative predicted rates
+            if my_lambda < 10**-10:
+                my_lambda = 10**-10
+            #Poisson term. Ignore the factorial piece which will cancel in likelihood ratio
+            to_return = to_return + (obs[i]*np.log(my_lambda) - my_lambda)
+
+        #Gaussian nuisance term
+        nuisance_term = -0.5*np.dot(np.dot(expected_nuis_arr,inv_x_signal_cov),expected_nuis_arr)
+        to_return = to_return + nuisance_term
+        return -to_return
+
+    def jac_min_log_numerator(expected_nuis_arr):
+        #expected_nuis_arr is the array of systematic background uncertainty nuisance parameters
+        #These are event rate densities
+        expected_nuis_arr = np.array(expected_nuis_arr)
+        to_return = np.array([0.])
+        #Poisson terms
+        #Poisson lambda. Have to rescale nuisance constribution by bin width
+        my_lambda = expected+expected_nuis_arr
+        dmy_lambda = np.array([1.])
+        #Prevent negative predicted rates
+        my_lambda[my_lambda < 10**-10] = np.ones(len(my_lambda[my_lambda < 10**-10])) * 10**-10
+        dmy_lambda[my_lambda < 10**-10] = 0
+        #Poisson term. Ignore the factorial piece which will cancel in likelihood ratio
+        to_return = to_return + (obs*dmy_lambda/my_lambda - dmy_lambda)
+        #Gaussian nuisance term
+        nuisance_term = -np.dot(inv_x_signal_cov,expected_nuis_arr)
+        to_return = to_return + nuisance_term
+        return -to_return
+
+    #Initialization of nuisance params
+    expected_nuis_array_init = [0.02]
+
+    #shift log likelihood to heklp minimization algo
+    def rescaled_min_log_numerator(expected_nuis_arr):
+        return min_log_numerator(expected_nuis_arr) - min_log_numerator(expected_nuis_array_init)
+
+    #Perform minimization over nuisance parameters. Set bounds for bg nuisance at around 8 sigma.
+    bnds=[[-8*y_unc[mask[0]],8*y_unc[mask[0]]]]
+    minimize_log_numerator = minimize(rescaled_min_log_numerator,
+                                      expected_nuis_array_init,
+                                      jac=jac_min_log_numerator,
+                                      bounds=bnds)
+
+    if verbose:
+        print("numerator: ",  minimize_log_numerator.items(),'\n')
+
+    #Now get likelihood ratio denominator
+    def min_log_denom(nuis_arr):
+        #nuis_arr contains the bg systematics and also the signal rate
+        expected_nuis_arr = np.array(nuis_arr)[:1]
+        #print(expected_nuis_arr)
+        mu = nuis_arr[1]
+        #Signal prediction
+        pred = [mu]
+        to_return = 0
+        #Poisson terms
+        for i, expected_nuis in enumerate(expected_nuis_arr):
+            #Poisson lambda
+            my_lambda = expected[i]+expected_nuis_arr[i] + pred[i]
+            #Prevent prediction from going negative
+            if my_lambda < 10**-10:
+                my_lambda = 10**-10
+            #Poisson term. Ignore the factorial piece which will cancel in likelihood ratio
+            to_return = to_return + (obs[i]*np.log(my_lambda) - my_lambda)
+
+        #Gaussian nuisance term
+        nuisance_term = -0.5*np.dot(np.dot(expected_nuis_arr,inv_x_signal_cov),expected_nuis_arr)
+        to_return = to_return + nuisance_term
+        return -to_return
+
+    def jac_min_log_denom(nuis_arr):
+        #expected_nuis_arr is the array of systematic background uncertainty nuisance parameters
+        #These are event rate densities
+        expected_nuis_arr = np.array(nuis_arr)[:1]
+        mu = nuis_arr[1]
+        pred = [mu]
+        to_return_first = np.array([0.])
+        #Poisson terms
+        #Poisson lambda. Have to rescale nuisance constribution by bin width
+        my_lambda = expected+expected_nuis_arr+pred
+        dmy_lambda = np.array([1.])
+        #Prevent prediction from going negative
+        my_lambda[my_lambda < 10**-10] = np.ones(len(my_lambda[my_lambda < 10**-10])) * 10**-10
+        dmy_lambda[my_lambda < 10**-10] = 0
+        #Poisson term. Ignore the factorial piece which will cancel in likelihood ratio
+        to_return_first = to_return_first + (obs*dmy_lambda/my_lambda - dmy_lambda)
+        #Gaussian nuisance term
+        nuisance_term = -np.dot(inv_x_signal_cov,expected_nuis_arr)
+        to_return_first = to_return_first + nuisance_term
+
+        to_return_last = np.array([0.])
+
+        dpred = np.array([[1.]])
+
+        my_lambda = expected+expected_nuis_arr+pred
+        dmy_lambda = dpred
+        to_return_last = np.dot((obs/my_lambda),dmy_lambda.T) - np.sum(dmy_lambda,axis=1)
+
+        return -np.append(to_return_first, to_return_last)
+
+    #initizalization for minimization
+    nuis_array_init = [0.01,1.]
+
+    #Shift log likelihood for helping minimization algo.
+    def rescaled_min_log_denom(nuis_arr):
+        return min_log_denom(nuis_arr) - min_log_denom(nuis_array_init)
+
+    bnds = ((None,None),(None,None))
+    minimize_log_denominator = minimize(rescaled_min_log_denom,nuis_array_init,
+                                        jac=jac_min_log_denom,
+                                        bounds=bnds)
+
+    if verbose:
+        print("Denominator: ",  minimize_log_denominator.items(),'\n')
+
+    if minimize_log_denominator.x[-1] < 0:
+        Zval = 0
+        neglognum = 0
+        neglogden = 0
+    else:
+        neglognum = min_log_numerator(minimize_log_numerator.x)
+        neglogden = min_log_denom(minimize_log_denominator.x)
+        Zval = np.sqrt(2*(neglognum - neglogden))
+
+
+    p0 = 1-norm.cdf(Zval)
+
+    if verbose:
+        print("z = ", Zval)
+        print("p0 = ", p0)
+
+    #plt.title(str(p0))
+#     if plotfile == 'show':
+#         plt.show()
+#     elif plotfile:
+#         plt.savefig(plotfile)
+
+    if return_teststat:
+        return p0, 2*(neglognum - neglogden)
+    else:
+        return p0
--- statistics/significance_fit.py
+++ statistics/significance_fit.py
@@ -1,12 +1,14 @@
-# https://github.com/violatingcp/QUASAR/blob/master/statistics/significance_fit.py
-#! /usr/bin/env python
+#!/usr/bin/env python
 import ROOT as r,sys,math,array,os
 from optparse import OptionParser
 from ROOT import std,RooDataHist
 from array import array
 import numpy as np
-#from stats import *
+from scipy.stats import poisson, norm, kstest
+from pvalue import *
+
 fOutput="Output.root"
+fHists=[]
 
 def end():
     if __name__ == '__main__':
@@ -16,20 +18,40 @@ def end():
             if 1 < len(rep):
                 rep = rep[0]
 
-def drawFrame(iX,iData,iFuncs,iCat):
+def drawFrame(iX,iData,iBkg,iFuncs,iCat):
     lCan   = r.TCanvas("qcd_"+iCat,"qcd_"+iCat,800,600)
+    leg = r.TLegend(0.55,0.63,0.86,0.87)
     lFrame = iX.frame()
     lFrame.SetTitle("")
     lFrame.GetXaxis().SetTitle("m_{jj} (GeV)")
+    lFrame.GetYaxis().SetTitle("Events")
+    iBkg.plotOn(lFrame,r.RooFit.FillColor(r.TColor.GetColor(100, 192, 232)),r.RooFit.FillStyle(3008), r.RooFit.DrawOption("E3"), r.RooFit.LineColor(r.kBlue))
     iData.plotOn(lFrame)
     iColor=51
-    for pFunc in iFuncs:
-        pFunc.plotOn(lFrame,r.RooFit.LineColor(iColor),r.RooFit.LineStyle(iColor != 50+1))
+    lRange = len(iFuncs)
+    for i0 in range(lRange):
+        if i0 == 2:
+            iFuncs[i0].plotOn(lFrame,r.RooFit.LineColor(r.kGreen+1))
+        else:
+            iFuncs[i0].plotOn(lFrame,r.RooFit.LineColor(iColor),r.RooFit.LineStyle(r.kDashed))
         iColor+=10
+    leg.SetFillColor(0)
     lFrame.Draw()
+    lTmpData  = r.TH1F("tmpData" ,"tmpData" ,1,0,10); lTmpData .SetMarkerStyle(r.kFullCircle);
+    lTmpBkg   = r.TH1F("tmpBkg"  ,"tmpBkg"  ,1,0,10); lTmpBkg  .SetFillStyle(3008); lTmpBkg.SetLineColor(r.kBlue); lTmpBkg.SetFillColor(r.TColor.GetColor(100, 192, 232));
+    lTmpFunc1 = r.TH1F("tmpFunc1","tmpFunc1",1,0,10); lTmpFunc1.SetLineColor(51);                lTmpFunc1.SetLineWidth(2); lTmpFunc1.SetLineStyle(r.kDashed);
+    lTmpFunc2 = r.TH1F("tmpFunc2","tmpFunc2",1,0,10); lTmpFunc2.SetLineColor(61);                lTmpFunc2.SetLineWidth(2); lTmpFunc2.SetLineStyle(r.kDashed);
+    lTmpFunc3 = r.TH1F("tmpFunc3","tmpFunc3",1,0,10); lTmpFunc3.SetLineColor(r.kGreen+1);        lTmpFunc3.SetLineWidth(2); #lTmpFunc3.SetLineStyle(r.kDashed);
+    leg.AddEntry(lTmpData,"data","lpe")
+    leg.AddEntry(lTmpBkg ,"loss-sideband data","f")
+    leg.AddEntry(lTmpFunc2,"bkg","lp")
+    leg.AddEntry(lTmpFunc3,"sig+bkg","lp")
+    leg.AddEntry(lTmpFunc1,"loss-sideband","lp")
+    leg.Draw()
     lCan.Modified()
     lCan.Update()
     lCan.SaveAs(lCan.GetName()+".png")
+    end()
 
 # build workspace
 def workspace(iOutput,iDatas,iFuncs,iCat="cat0"):
@@ -47,40 +69,78 @@ def workspace(iOutput,iDatas,iFuncs,iCat="cat0"):
         lW.writeToFile(iOutput)
     return lW
 
+def clip(iData,iMin,iMax):
+    pMinBin = 0
+    pMaxBin = iData.GetNbinsX()
+    for i0 in range(iData.GetNbinsX()+1):
+        pLVal = iData.GetBinLowEdge(i0)
+        pHVal = iData.GetBinLowEdge(i0)
+        if iMin > pLVal:
+            pMinBin = i0
+        if iMax > pHVal:
+            pMaxBin = i0
+    NBins = pMaxBin-pMinBin
+    pMinLow = iData.GetBinLowEdge(pMinBin)
+    pMinMax = iData.GetBinLowEdge(pMaxBin)
+    pData = r.TH1F(iData.GetName()+"R",iData.GetName()+"R",NBins,pMinLow,pMinMax)
+    for i0 in range(NBins):
+        print(iData.GetBinLowEdge(i0+pMinBin),pData.GetBinLowEdge(i0+1),"! Done")
+        pData.SetBinContent(i0+1,iData.GetBinContent(i0+pMinBin))
+    fHists.append(pData)
+    return pData
 
-def fitFunc(iData,iCat,iMin=3000,iMax=6000,iStep=150):
-    lXMin=iData.GetXaxis().GetXmin()
-    lXMax=iData.GetXaxis().GetXmax()
-    lNBins=iData.GetNbinsX()
+def fitFunc(iData,iBkg,iCat,iMin=3000,iMax=6000,iStep=150,iFixToSB=False):
+    pData = clip(iData,3200,6500)
+    pBkg  = clip(iBkg ,3200,6500)
+    #pBkg  = iBkg#clip(iBkg ,3000,6200)
+    lXMin=pData.GetXaxis().GetXmin()
+    lXMax=pData.GetXaxis().GetXmax()
+    lNBins=pData.GetNbinsX()
     lX = r.RooRealVar("x","x",lXMin,lXMax)
     lX.setBins(lNBins)
-    lNTot   = r.RooRealVar("qcdnorm_"+iCat,"qcdnorm_"+iCat,iData.Integral(),0,3*iData.Integral())
-    lA0     = r.RooRealVar   ("a0"+"_"+iCat,"a0"+"_"+iCat,0.00,-1.,1.)
-    lA1     = r.RooRealVar   ("a1"+"_"+iCat,"a1"+"_"+iCat,0.01,-1,1.)
-    lA2     = r.RooRealVar   ("a2"+"_"+iCat,"a2"+"_"+iCat,0.01,-1,1)
-    lA3     = r.RooRealVar   ("a3"+"_"+iCat,"a3"+"_"+iCat,0.01,-1,1)
-    lA4     = r.RooRealVar   ("a4"+"_"+iCat,"a4"+"_"+iCat,0.01,-1,1)
-    lA5     = r.RooRealVar   ("a5"+"_"+iCat,"a5"+"_"+iCat,0.01,-1,1)
-    #lQFuncP = r.RooBernstein("tqcd_pass_"+iCat,"tqcd_pass_"+iCat,iX,r.RooArgList(lA0,lA1,lA2,lA3))
-    lQFuncP = r.RooBernstein("tqcd_pass_"+iCat,"tqcd_pass_"+iCat,lX,r.RooArgList(lA0,lA1,lA2,lA3))#,lA5))
+    lNTot   = r.RooRealVar("qcdnorm_"+iCat,"qcdnorm_"+iCat,pData.Integral(),0,3*pData.Integral())
+    #lA0     = r.RooRealVar   ("a0"+"_"+iCat,"a0"+"_"+iCat,0.00,-1.,1.)
+    #lA1     = r.RooRealVar   ("a1"+"_"+iCat,"a1"+"_"+iCat,0.01,-1,1.)
+    #lA2     = r.RooRealVar   ("a2"+"_"+iCat,"a2"+"_"+iCat,0.01,-1,1)
+    #lA3     = r.RooRealVar   ("a3"+"_"+iCat,"a3"+"_"+iCat,0.01,-1,1)
+    #lA4     = r.RooRealVar   ("a4"+"_"+iCat,"a4"+"_"+iCat,0.01,-1,1)
+    #lA5     = r.RooRealVar   ("a5"+"_"+iCat,"a5"+"_"+iCat,0.01,-1,1)
+    #lQFuncP = r.RooBernstein("tqcd_pass_"+iCat,"tqcd_pass_"+iCat,lX,r.RooArgList(lA0,lA1,lA2,lA3))#,lA5))
+
+    lA0      = r.RooRealVar   ("a0"+"_"+iCat,"a0"+"_"+iCat,1.0,-200.,200.); lA0.setConstant(r.kTRUE)
+    lA1      = r.RooRealVar   ("a1"+"_"+iCat,"a1"+"_"+iCat,1.00,-200.,200.)
+    lA2      = r.RooRealVar   ("a2"+"_"+iCat,"a2"+"_"+iCat,3.00,-200.,200.)
+    lQFuncP  = r.RooGenericPdf("tqcd_pass_"+iCat,"tqcd_pass_"+iCat,"(1-@0/13000.)**@2*(@1/13000.)**-@2",r.RooArgList(lX,lA1,lA2))#,lA5))
     lQCDP   = r.RooExtendPdf("qcd_"+iCat, "qcd"+iCat,lQFuncP,lNTot)
 
-    lMass   = r.RooRealVar("mass","mass"  ,3875,3000,6000); lMass.setConstant(r.kTRUE)
-    lSigma  = r.RooRealVar("sigma","Width of Gaussian",60,20,500); lSigma.setConstant(r.kTRUE)
+    lBNTot   = r.RooRealVar("bqcdnorm_"+iCat,"bqcdnorm_"+iCat,pData.Integral(),0,3*pData.Integral())
+    lBA0      = r.RooRealVar   ("ba0"+"_"+iCat,"ba0"+"_"+iCat,0.00,-200.,200.)
+    lBA1      = r.RooRealVar   ("ba1"+"_"+iCat,"ba1"+"_"+iCat,0.00,-200.,200.)
+    lBA2      = r.RooRealVar   ("ba2"+"_"+iCat,"ba2"+"_"+iCat,0.00,-200.,200.)
+    lBQFuncP  = r.RooGenericPdf("btqcd_pass_"+iCat,"btqcd_pass_"+iCat,"(1-@0/13000.)**@1*(@0/13000.)**-@2",r.RooArgList(lX,lBA1,lBA2))
+    lBQCDP    = r.RooExtendPdf ("bqcd_"+iCat, "bqcd"+iCat,lBQFuncP,lBNTot)
+
+    lMass   = r.RooRealVar("mass","mass"  ,5000,3000,7000); lMass.setConstant(r.kTRUE)
+    lSigma  = r.RooRealVar("sigma","Width of Gaussian",80,10,500); lSigma.setConstant(r.kTRUE)
     lGaus   = r.RooGaussian("gauss","gauss(x,mean,sigma)",lX,lMass,lSigma)
-    lNSig   = r.RooRealVar("signorm_"+iCat,"signorm_"+iCat,iData.Integral()*0.05,0,0.3*iData.Integral())
+    lNSig   = r.RooRealVar("signorm_"+iCat,"signorm_"+iCat,0.1*pData.Integral(),0,0.3*pData.Integral())
     lSig    = r.RooExtendPdf("sig_"+iCat, "sig_"+iCat,lGaus,lNSig)
     lTot    = r.RooAddPdf("model", "model", r.RooArgList(lSig, lQCDP))
+    lHData  = r.RooDataHist("data_obs","data_obs", r.RooArgList(lX),pData)
+    lHBkg   = r.RooDataHist("bkgestimate","bkgestimate", r.RooArgList(lX),pBkg)
 
-    lHData  = r.RooDataHist("data_obs","data_obs", r.RooArgList(lX),iData)
-    lTot.fitTo(lHData,r.RooFit.Extended(r.kTRUE))#,r.RooFit.PrintLevel(-1))
-    drawFrame(lX,lHData,[lTot,lQCDP],iCat)
+    lBQCDP.fitTo(lHBkg)
+    lQCDP.fitTo(lHBkg);
+    if iFixToSB:
+        lA1.setConstant(r.kTRUE); lA2.setConstant(r.kTRUE);
+    lTot.fitTo(lHData)#,r.RooFit.Extended(r.kTRUE))
+    drawFrame(lX,lHData,lHBkg,[lBQCDP,lQCDP,lTot],iCat)
 
     lW = workspace(fOutput,[lHData],[lTot,lQCDP],iCat)
     lW.defineSet("poi","signorm_"+iCat)
     bmodel = r.RooStats.ModelConfig("b_model",lW)
     bmodel.SetPdf(lW.pdf("model"))
-    bmodel.SetNuisanceParameters(r.RooArgSet(lA0,lA1,lA2,lA3,lNTot))
+    bmodel.SetNuisanceParameters(r.RooArgSet(lA1,lA2,lNTot))
     bmodel.SetObservables(r.RooArgSet(lX))
     bmodel.SetParametersOfInterest(lW.set("poi"))
     lW.var("signorm_"+iCat).setVal(0)
@@ -88,7 +148,7 @@ def fitFunc(iData,iCat,iMin=3000,iMax=6000,iStep=150):
 
     sbmodel = r.RooStats.ModelConfig("s_model",lW)
     sbmodel.SetPdf(lW.pdf("model"))
-    sbmodel.SetNuisanceParameters(r.RooArgSet(lA0,lA1,lA2,lA3,lNTot))
+    sbmodel.SetNuisanceParameters(r.RooArgSet(lA1,lA2,lNTot))
     sbmodel.SetObservables(r.RooArgSet(lX))
     sbmodel.SetParametersOfInterest(lW.set("poi"))
     lW.var("signorm_"+iCat).setVal(lNSig.getVal())
@@ -105,51 +165,114 @@ def fitFunc(iData,iCat,iMin=3000,iMax=6000,iStep=150):
         ac.SetPrintLevel(-1)
         asResult = ac.GetHypoTest()
         pvalue=asResult.NullPValue()
-        masses.append(mass)
-        pvalues.append(pvalue)
-        print(mass,pvalue)
+        if pvalue > 1e-8:
+            masses.append(mass)
+            pvalues.append(pvalue)
+            print(mass,pvalue)
     return masses,pvalues
 
 def setupData(iFileName):
     lDatas=[]
     lFile = r.TFile(iFileName)
     lH    = lFile.Get("data_obs")
+    lH2   = lFile.Get("bkgestimate")
+
     lH.SetDirectory(0)
+    lH2.SetDirectory(0)
     for i1 in range(lH.GetNbinsX()+1):
         lH.SetBinError(i1,math.sqrt(lH.GetBinContent(i1)))
+        lH2.SetBinError(i1,math.sqrt(lH2.GetBinContent(i1)))
     lFile.Close()
-    return lH
 
-def sigVsMassPlot(masses,pvalues):
+    return lH, lH2
+
+def sigVsMassPlot(masses,pvalues,labels):
     lC0 = r.TCanvas("A","A",800,600)
-    graph1 = r.TGraph(len(masses),masses,pvalues)
-    graph1.SetMarkerStyle(20)
-    graph1.GetXaxis().SetTitle("mass")
-    graph1.GetYaxis().SetTitle("p0 value")
-    graph1.SetTitle("Significance vs Mass")
-    graph1.SetLineColor(4)
-    graph1.SetMarkerColor(4)
-    r.gPad.SetLogy(True)
-    graph1.GetYaxis().SetRangeUser(1e-8,1.0)
-    sigmas=[0.317,0.045,0.0027,0.0000633721,0.0000005742]
-    graph1.Draw("alp")
+    leg = r.TLegend(0.55,0.23,0.86,0.47)
+    leg.SetFillColor(0)
+
+    lGraphs=[]
+    sigmas=[]
+    for i0 in range(len(masses)):
+        graph1 = r.TGraph(len(masses[i0]),masses[i0],pvalues[i0])
+        graph1.SetMarkerStyle(20)
+        graph1.GetXaxis().SetTitle("m_{jj} (GeV)")
+        graph1.GetYaxis().SetTitle("p^{0} value")
+        graph1.SetTitle("Significance vs Mass")
+        graph1.SetLineColor(2+i0)
+        graph1.SetMarkerColor(2+i0)
+        graph1.SetLineWidth(2+i0)
+        r.gPad.SetLogy(True)
+        graph1.GetYaxis().SetRangeUser(1e-8,1.0)
+        if i0 == 0:
+            graph1.Draw("alp")
+        else:
+            graph1.Draw("lp")
+        lGraphs.append(graph1)
+        leg.AddEntry(graph1,labels[i0],"lp")
+
+    #sigmas=[0.317,0.045,0.0027,0.0000633721,0.0000005742]
     lines=[]
-    for i0 in range(len(sigmas)):
-        lLine = r.TLine(masses[0],sigmas[i0],masses[len(masses)-1],sigmas[i0])
+    for i0 in range(5):#len(sigmas)):
+        sigmas.append(1-norm.cdf(i0+1))
+        lLine = r.TLine(masses[0][0],sigmas[i0],masses[0][len(masses[0])-1],sigmas[i0])
         lLine.SetLineStyle(r.kDashed)
         lLine.SetLineWidth(2)
         lLine.Draw()
-        #lPT = r.TPaveText(masses[0+2],sigmas[i0],masses[len(masses)],sigmas[i0]+0.01)
-        #lPT.AddText(str(i0)+"#sigma")
-        #lPT.Draw()
+        lPT = r.TPaveText(3500,sigmas[i0],4000,sigmas[i0]+1.5*sigmas[i0])
+        lPT.SetFillStyle(4050)
+        lPT.SetFillColor(0)
+        lPT.SetBorderSize(0)
+        lPT.AddText(str(i0+1)+"#sigma")
+        lPT.Draw()
         lines.append(lLine)
-        #lines.append(lPT)
+        lines.append(lPT)
+
+    for pGraph in lGraphs:
+        pGraph.Draw("lp")
+
+    leg.Draw()
     lC0.Update()
     lC0.Draw()
     lC0.SaveAs("pvalue.png")
     end()
 
+def pvalue(iData):
+    bins=[]
+    data=[]
+    pData = clip(iData,3200,6500)
+    for i0 in range(pData.GetNbinsX()+1):
+        bins.append(pData.GetBinLowEdge(i0+1))
+        if i0 < pData.GetNbinsX():
+            data.append(pData.GetBinContent(i0+1))
+    masks       = [[bin_i,bin_i, bin_i] for bin_i in range(1,len(bins)-2)]
+    pvalues_in = [get_p_value(data,bins,mask=mask,verbose=0,plotfile=None) for i, mask in enumerate(masks)]
+    masses_in  = [0.5*(bins[i] + bins[i+1]) for i in range(len(bins)-1)]
+    masses =  array( 'd' )
+    pvalues = array( 'd' )
+    print("!!!!",len(masses_in),len(pvalues_in))
+    for i0 in range(len(pvalues_in)):
+        masses.append(masses_in[i0])
+        pvalues.append(pvalues_in[i0])
+    return masses,pvalues
+
 if __name__ == "__main__":
-    lData        = setupData("blackbox1.root")
-    masses,pvalues=fitFunc(lData,"test",3000,6000,300)
-    sigVsMassPlot(masses,pvalues)
+    lData1, lBkg1        = setupData("blackbox2-REFINE.root")
+    lData2, lBkg2        = setupData("blackbox2-WAIC.root")
+    iBkgTemp=True
+    masses1,pvalues1=fitFunc(lData1,lBkg1,"WAIC",3500,6000,100,iBkgTemp)
+    masses2,pvalues2=fitFunc(lData2,lBkg2,"bb2-REFINE",3500,6000,100,iBkgTemp)
+    masses3,pvalues3=pvalue(lData1)
+    labels=[]
+    masses=[]
+    pvalues=[]
+    labels.append("REFINE")
+    labels.append("WAIC")
+    labels.append("Masked-REFINE")
+    pvalues.append(pvalues1)
+    pvalues.append(pvalues2)
+    pvalues.append(pvalues3)
+    masses.append(masses1)
+    masses.append(masses2)
+    masses.append(masses3)
+    sigVsMassPlot(masses,pvalues,labels)
